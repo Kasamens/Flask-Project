f29b6681372cdb820c7c8031d88014c6287ca3c7
Kasamens <kwadwoasamoa7@gmail.com>
2018-07-12 19:48:39 +0000

    Updating files

--- myapp.py
+++ myapp.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('index.html')
-	
 """	
 @app.route('/hello')
 def hello():
@@ -14,5 +13,6 @@ def hello():
 @app.route('/foo/<name>')
 def foo(name):
 	return render_template('foo.html', to=name)	
-	
-"""
+"""	
+if __name__ == "__main__":
+	app.run(debug=True, host = '0.0.0.0')
