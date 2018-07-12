d026f25eec5893b7acfc05526f3d4d25da81cdbf
Kasamens <kwadwoasamoa7@gmail.com>
2018-07-12 22:48:05 +0000

    Sanity check

--- myapp.py
+++ myapp.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('index.html')
-"""	
-@app.route('/hello')
-def hello():
-	return 'Hello World'
+
+#@app.route('/hello')
+#def hello():
+#	return 'Hello World'
 	
-@app.route('/foo/<name>')
-def foo(name):
-	return render_template('foo.html', to=name)	
-"""	
+#@app.route('/foo/<name>')
+#def foo(name):
+#	return render_template('foo.html', to=name)	
+
 if __name__ == "__main__":
 	app.run(debug=True)
