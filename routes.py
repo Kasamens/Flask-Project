ea789c3ed12c8b6f98055b6db7d9543d1c1742ff
Kasamens <kwadwoasamoa7@gmail.com>
2018-07-12 18:20:03 +0000

    Adding initial files

---
+++ routes.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+	
+@app.route('/hello')
+def hello():
+	return 'Hello World'
+	
+@app.route('/foo/<name>')
+def foo(name):
+	return render_template('foo.html', to=name)	
+	
+if __name__ == "__main__":
+	app.run(debug=True, host = '0.0.0.0')
