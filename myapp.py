35f129aa5c14edcc535242e211faa7f8caf9c874
Ben Evans <benjamin.john.evans@gmail.com>
2018-07-13 16:19:46 +0000

    Fix heroku config for flask

--- myapp.py
+++ myapp.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 #@app.route('/hello')
 #def hello():
@@ -15,4 +16,6 @@ def index():
 #	return render_template('foo.html', to=name)	
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+#    app.run(debug=True, port=33507)
